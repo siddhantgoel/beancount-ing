a487ddb38f926b314573aac21f739ebe07afe1a5
Siddhant Goel <me@sgoel.org>
2019-07-08 17:55:07 -0400

    remove locale-based number parsing

--- beancount_ing_diba/ec.py
+++ beancount_ing_diba/ec.py
@@ -1,7 +1,5 @@
-from contextlib import contextmanager
 import csv
 from datetime import datetime
-import locale
 import re
 
 from beancount.core.amount import Amount
@@ -35,34 +33,22 @@ class InvalidFormatError(Exception):
     pass
 
 
-@contextmanager
-def _change_locale(key, value):
-    original = locale.getlocale(key)
-
-    try:
-        locale.setlocale(key, value)
-        yield
-    finally:
-        locale.setlocale(key, original)
-
-
 def _format_iban(iban):
     return re.sub(r'\s+', '', iban, flags=re.UNICODE)
 
 
+def _format_number_de(value: str) -> Decimal:
+    thousands_sep = '.'
+    decimal_sep = ','
+
+    return Decimal(value.replace(thousands_sep, '').replace(decimal_sep, '.'))
+
+
 class ECImporter(importer.ImporterProtocol):
-    def __init__(
-        self,
-        iban,
-        account,
-        user,
-        numeric_locale='de_DE.UTF-8',
-        file_encoding='ISO-8859-1',
-    ):
+    def __init__(self, iban, account, user, file_encoding='ISO-8859-1'):
         self.iban = _format_iban(iban)
         self.account = account
         self.user = user
-        self.numeric_locale = numeric_locale
         self.file_encoding = file_encoding
 
         self._date_from = None
@@ -123,141 +109,131 @@ class ECImporter(importer.ImporterProtocol):
         entries = []
         line_index = 0
 
-        with _change_locale(locale.LC_NUMERIC, self.numeric_locale):
-            with open(file_.name, encoding=self.file_encoding) as fd:
-                # Header - first line
-                line = fd.readline().strip()
+        with open(file_.name, encoding=self.file_encoding) as fd:
+            # Header - first line
+            line = fd.readline().strip()
+            line_index += 1
+
+            if not self._is_valid_first_header(line):
+                raise InvalidFormatError()
+
+            # Header - second line
+            line = fd.readline().strip()
+            line_index += 1
+
+            if not self._is_valid_second_header(line):
+                raise InvalidFormatError()
+
+            # Empty line
+            line = fd.readline().strip()
+            line_index += 1
+
+            if line:
+                raise InvalidFormatError()
+
+            # Meta
+            lines = [fd.readline().strip() for _ in range(6)]
+
+            reader = csv.reader(
+                lines, delimiter=';', quoting=csv.QUOTE_MINIMAL, quotechar='"'
+            )
+
+            for line in reader:
+                key, *values = line
                 line_index += 1
 
-                if not self._is_valid_first_header(line):
-                    raise InvalidFormatError()
+                if key == 'IBAN':
+                    if _format_iban(values[0]) != self.iban:
+                        raise InvalidFormatError()
+                elif key == 'Bank':
+                    if values[0] not in BANKS:
+                        raise InvalidFormatError()
+                elif key == 'Kunde':
+                    if values[0] != self.user:
+                        raise InvalidFormatError()
+                elif key == 'Zeitraum':
+                    splits = values[0].strip().split(' - ')
 
-                # Header - second line
-                line = fd.readline().strip()
-                line_index += 1
+                    if len(splits) != 2:
+                        raise InvalidFormatError()
 
-                if not self._is_valid_second_header(line):
-                    raise InvalidFormatError()
+                    self._date_from = datetime.strptime(
+                        splits[0], '%d.%m.%Y'
+                    ).date()
+                    self._date_to = datetime.strptime(
+                        splits[1], '%d.%m.%Y'
+                    ).date()
+                elif key == 'Saldo':
+                    amount, currency = values
 
-                # Empty line
-                line = fd.readline().strip()
-                line_index += 1
+                    self._balance = Amount(_format_number_de(amount), currency)
 
-                if line:
-                    raise InvalidFormatError()
+            # Empty line
+            line = fd.readline().strip()
+            line_index += 1
 
-                # Meta
-                lines = [fd.readline().strip() for _ in range(6)]
+            if line:
+                raise InvalidFormatError()
 
-                reader = csv.reader(
-                    lines,
-                    delimiter=';',
-                    quoting=csv.QUOTE_MINIMAL,
-                    quotechar='"',
-                )
+            # Pre-header line
+            line = fd.readline().strip()
+            line_index += 1
 
-                for line in reader:
-                    key, *values = line
-                    line_index += 1
+            if line != PRE_HEADER:
+                raise InvalidFormatError()
 
-                    if key == 'IBAN':
-                        if _format_iban(values[0]) != self.iban:
-                            raise InvalidFormatError()
-                    elif key == 'Bank':
-                        if values[0] not in BANKS:
-                            raise InvalidFormatError()
-                    elif key == 'Kunde':
-                        if values[0] != self.user:
-                            raise InvalidFormatError()
-                    elif key == 'Zeitraum':
-                        splits = values[0].strip().split(' - ')
+            # Empty line
+            line = fd.readline().strip()
+            line_index += 1
 
-                        if len(splits) != 2:
-                            raise InvalidFormatError()
+            if line:
+                raise InvalidFormatError()
 
-                        self._date_from = datetime.strptime(
-                            splits[0], '%d.%m.%Y'
-                        ).date()
-                        self._date_to = datetime.strptime(
-                            splits[1], '%d.%m.%Y'
-                        ).date()
-                    elif key == 'Saldo':
-                        amount, currency = values
+            # Data entries
+            reader = csv.reader(
+                fd, delimiter=';', quoting=csv.QUOTE_MINIMAL, quotechar='"'
+            )
 
-                        self._balance = Amount(
-                            locale.atof(amount, Decimal), currency
-                        )
+            for line in reader:
+                if line == list(FIELDS):
+                    continue
 
-                # Empty line
-                line = fd.readline().strip()
-                line_index += 1
+                (
+                    date,  # Buchung
+                    _,  # Valuta
+                    payee,  # Auftraggeber/Empfänger
+                    booking_text,  # Buchungstext
+                    description,  # Verwendungszweck
+                    _,  # Saldo
+                    _,  # Währung
+                    amount,  # Betrag
+                    currency,  # Währung
+                ) = line
 
-                if line:
-                    raise InvalidFormatError()
+                meta = data.new_metadata(file_.name, line_index)
 
-                # PreHeader line
-                line = fd.readline().strip()
-                line_index += 1
+                amount = Amount(_format_number_de(amount), currency)
+                date = datetime.strptime(date, '%d.%m.%Y').date()
 
-                if line != PRE_HEADER:
-                    raise InvalidFormatError()
+                description = '{} {}'.format(booking_text, description).strip()
 
-                # Empty line
-                line = fd.readline().strip()
-                line_index += 1
+                postings = [
+                    data.Posting(self.account, amount, None, None, None, None)
+                ]
 
-                if line:
-                    raise InvalidFormatError()
-
-                # Data entries
-                reader = csv.reader(
-                    fd, delimiter=';', quoting=csv.QUOTE_MINIMAL, quotechar='"'
-                )
-
-                for line in reader:
-                    if line == list(FIELDS):
-                        continue
-
-                    (
-                        date,  # Buchung
-                        _,  # Valuta
-                        payee,  # Auftraggeber/Empfänger
-                        booking_text,  # Buchungstext
-                        description,  # Verwendungszweck
-                        _,  # Saldo
-                        _,  # Währung
-                        amount,  # Betrag
-                        currency,  # Währung
-                    ) = line
-
-                    meta = data.new_metadata(file_.name, line_index)
-
-                    amount = Amount(locale.atof(amount, Decimal), currency)
-                    date = datetime.strptime(date, '%d.%m.%Y').date()
-
-                    description = '{} {}'.format(
-                        booking_text, description
-                    ).strip()
-
-                    postings = [
-                        data.Posting(
-                            self.account, amount, None, None, None, None
-                        )
-                    ]
-
-                    entries.append(
-                        data.Transaction(
-                            meta,
-                            date,
-                            self.FLAG,
-                            payee,
-                            description,
-                            data.EMPTY_SET,
-                            data.EMPTY_SET,
-                            postings,
-                        )
+                entries.append(
+                    data.Transaction(
+                        meta,
+                        date,
+                        self.FLAG,
+                        payee,
+                        description,
+                        data.EMPTY_SET,
+                        data.EMPTY_SET,
+                        postings,
                     )
+                )
 
-                    line_index += 1
+                line_index += 1
 
-            return entries
+        return entries
